ab3fa330457664cd827530ccd8c9755800c7ebf4
linmq <leolmq@gmail.com>
2016-03-20 15:21:51 +0800

    Networking Player movement
    Networking Player movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,10 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
