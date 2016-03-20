5852e27a960c71b9a85578a57048b3ca390da7f3
linmq <leolmq@gmail.com>
2016-03-20 21:54:34 +0800

    Spawning and Respawning
    Spawning and Respawning

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -7,12 +7,22 @@ public class Health : NetworkBehaviour
 {
 
     public const int maxHealth = 100;
-
     public bool destroyOnDeath;
 
     [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
+
     public RectTransform healthBar;
+
+    private NetworkStartPosition[] spawnPoints;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
 
     public void TakeDamage(int amount)
     {
@@ -30,11 +40,10 @@ public class Health : NetworkBehaviour
             {
                 currentHealth = maxHealth;
 
+                // called on the Server, invoked on the Clients
                 RpcRespawn();
             }
         }
-
-        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
     }
 
     void OnChangeHealth(int health)
@@ -47,8 +56,17 @@ public class Health : NetworkBehaviour
     {
         if (isLocalPlayer)
         {
-            // move back to zero location
-            transform.position = Vector3.zero;
+            // Set the spawn point to origin as a default value
+            Vector3 spawnPoint = Vector3.zero;
+
+            // If there is a spawn point array and the array is not empty, pick one at random
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            // Set the player’s position to the chosen spawn point
+            transform.position = spawnPoint;
         }
     }
 }
